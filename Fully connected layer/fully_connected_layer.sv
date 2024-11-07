2dbc13e6013f71be00771d638a7ed4de77cd1c52
Maksim Ananev <65023680+Kobzon86@users.noreply.github.com>
2024-11-07 21:26:31 +0300

    Update fully_connected_layer.sv

--- Fully connected layer/fully_connected_layer.sv
+++ Fully connected layer/fully_connected_layer.sv
@@ -13,9 +13,9 @@
 
 module fully_connected_layer #(
     //data width parameters
-    parameter PIX_WIDTH          = 8  ,
-    parameter WEIGHT_WIDTH       = 10 ,
-    parameter WEIGHT_FRACT_WIDTH = 5  ,
+    parameter PIX_WIDTH          = 16  ,
+    parameter WEIGHT_WIDTH       = 16 ,
+    parameter WEIGHT_FRACT_WIDTH = 10  ,
     //array_parameter
     parameter IN_DIMENSION       = 200,
     parameter OUT_DIMENSION      = 64
@@ -35,29 +35,75 @@ module fully_connected_layer #(
     output logic                                      o_eop               ,
     ///
     input  int                                        weights_mem_in_data ,
-    input  int                                        weights_mem_in_addr ,
-    input  int                                        weights_mem_sel_addr,
+    input  [$clog2(IN_DIMENSION)-1:0]                 weights_mem_in_addr ,
+    input  [$clog2(OUT_DIMENSION):0]                  weights_mem_sel_addr,
     input                                             weights_mem_in_fc_wr,
     ///
     output logic                                      o_ready
 );
 
-logic [WEIGHT_WIDTH-1:0]weights[OUT_DIMENSION][IN_DIMENSION];
+
+
+
+int col_cntr;
+logic [OUT_DIMENSION :0] weight_wr;
+
+always_comb begin 
+   weight_wr = '0;
+   weight_wr[weights_mem_sel_addr] = weights_mem_in_fc_wr;
+end
+
+genvar y;
+
+logic [WEIGHT_WIDTH-1:0]weights[OUT_DIMENSION];
+
+generate 
+for(y = 0; y < OUT_DIMENSION; y++)begin
+    single_port_rom #(
+    .ADDR_WIDTH($clog2(IN_DIMENSION)),
+    .DATA_WIDTH(WEIGHT_WIDTH)
+    )
+    weight_rom(
+     .clk(clk),
+     .w_addr(weights_mem_in_addr),
+     .r_addr(col_cntr),
+     .data(weights_mem_in_data),
+     .o(weights[y]),
+     .we(weight_wr[y])
+    
+    );
+end
+
+endgenerate
+
+
+       
+
 logic [WEIGHT_WIDTH-1:0]bias[OUT_DIMENSION];
 
-
+logic [PIX_WIDTH-1:0] i_data_ff;
+logic i_sop_ff;
+logic o_ready_ff;
+logic i_valid_ff;
 always_ff @(posedge clk) begin
-    if(weights_mem_in_fc_wr)
-        if(weights_mem_sel_addr == OUT_DIMENSION)
-            bias[weights_mem_in_addr] <= weights_mem_in_data;
-        else
-            weights[weights_mem_sel_addr][weights_mem_in_addr] <= weights_mem_in_data;
+
+    if(weight_wr[OUT_DIMENSION])
+        bias[weights_mem_in_addr] <= weights_mem_in_data;
+
+    i_data_ff <= i_data;
+    i_valid_ff <= i_valid;
+    i_sop_ff <= i_sop;
+    o_ready_ff <= o_ready;
 
     if(~rst_n) begin
+        i_data_ff <= '0;
+        i_sop_ff <= '0;
+        o_ready_ff <= '0;
+        i_valid_ff <= '0;
     end
 end
 
-int col_cntr;
+
 
 logic [PIX_WIDTH*WEIGHT_FRACT_WIDTH+$clog2(OUT_DIMENSION)-1:0] integrators[OUT_DIMENSION];
 
@@ -70,13 +116,14 @@ typedef enum logic [2:0] {
 
 e_state state;
 
+logic fill_delay;
 
 always_ff @(posedge clk) begin
     if(clk_en) begin
 
-        o_valid <= 1'd0;
-
-        o_ready <= 1'b1;
+        o_valid    <= 1'd0;
+        o_ready    <= 1'b1;
+        fill_delay <= 1'b0;
 
         case (state)
             IDLE: begin
@@ -96,6 +143,7 @@ always_ff @(posedge clk) begin
                         state <= RELEASE;
                         col_cntr <= '0;
                         o_ready <= 1'b0;
+                        fill_delay <= 1'b1;
                     end
                 end
                 
@@ -106,9 +154,9 @@ always_ff @(posedge clk) begin
                     state <= IDLE;
                 end
                 else begin
-                    o_valid <= 1'b1;
                     o_data <= $signed(integrators[col_cntr])/(2**WEIGHT_FRACT_WIDTH) + $signed(bias[col_cntr]);
-                    col_cntr <= col_cntr + 'd1;
+                    o_valid <= !fill_delay;
+                    col_cntr <= col_cntr + $size(col_cntr)'(!fill_delay);
                 end
 
                 o_sop <= col_cntr == 'd0;
@@ -129,21 +177,42 @@ end
 
 always_ff @(posedge clk) begin
     if(clk_en) begin
-        if(i_valid && o_ready)
+        if(i_valid_ff && o_ready_ff)
 
             foreach (integrators[x]) begin
-                if(i_sop)
-                    integrators[x] <= $signed(weights[x][col_cntr])*$signed(i_data);
+                if(i_sop_ff)
+                    integrators[x] <= $signed(weights[x])*$signed(i_data_ff);
                 else
-                    integrators[x] <= $signed(weights[x][col_cntr])*$signed(i_data) + $signed(integrators[x]);
+                    integrators[x] <= $signed(weights[x])*$signed(i_data_ff) + $signed(integrators[x]);
             end
     end
 end
 
 
-
-
-
-
-
 endmodule : fully_connected_layer
+
+
+module single_port_rom
+  # (parameter ADDR_WIDTH = 4,
+     parameter DATA_WIDTH = 32
+    )
+
+  ( 	input 					clk,
+   		input [ADDR_WIDTH-1:0]	r_addr,
+   		input [ADDR_WIDTH-1:0]	w_addr,
+   		input  [DATA_WIDTH-1:0]	data,
+   		output logic [DATA_WIDTH-1:0]	o,
+   		input 					we
+  );
+
+  reg [DATA_WIDTH-1:0] 	mem [2**ADDR_WIDTH];
+
+  always @ (posedge clk) begin
+    if (we)
+      mem[w_addr] <= data;
+      
+    o <=  mem[r_addr];
+  end
+
+    
+endmodule
